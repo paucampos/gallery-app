3d06127bd3e2b7e99007732b6488cdbde8147612
Pau Campos <pau.campos89@gmail.com>
2024-02-07 12:40:56 -0300

    feat: add ionic to angular proyect

---
+++ src/app/pages/detail/detail.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-detail',
+  templateUrl: './detail.component.html'
+})
+export class DetailComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
